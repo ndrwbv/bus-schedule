f105a2d25200489cc5f3279af50e68443f98c1dc
Andrew Boev <ndrwbv@gmail.com>
2021-10-01 12:48:58 +0700

    adds ym

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { YMInitializer } from "react-yandex-metrika";
 // import reportWebVitals from './reportWebVitals';
 
+console.log(process.env.NODE_ENV)
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {process.env.NODE_ENV === "production" && (
+        <YMInitializer accounts={[85705234]} options={{ webvisor: true }} />
+      )}
   </React.StrictMode>,
   document.getElementById('root')
 );
