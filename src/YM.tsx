ef9dd0cc8d3aea4caefc1520b2faf8470dc0b2b9
Andrew Boev <ndrwbv@gmail.com>
2022-03-15 12:58:13 +0700

    fix metrics

--- src/YM.tsx
+++ src/YM.tsx
@@ -2,7 +2,7 @@ import config from 'configs/base'
 import { YMInitializer } from 'react-yandex-metrika'
 
 const YM = () => {
-	if (process.env.NODE_ENV === 'production') return null
+	if (process.env.NODE_ENV !== 'production') return null
 
 	return <YMInitializer accounts={[config.YANDEX_ID]} options={{ webvisor: true }} />
 }
