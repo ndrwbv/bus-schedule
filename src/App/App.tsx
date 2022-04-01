584b2ebe28bd8d0519c1775bcd9a6b37efff432f
Andrew Boev <ndrwbv@gmail.com>
2022-04-01 19:07:24 +0700

    adds logo

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,6 @@
 import { fetchInfo, fetchSchedule } from 'api'
+import Logo from 'components/Logo/Logo'
+import { LogoContainer, LogoInner } from 'components/Logo/styled'
 import ScheduleProvider from 'context/ScheduleContext'
 import { getNextDay } from 'helpers/schedule'
 import styled from 'styled-components'
@@ -8,6 +10,7 @@ const AppContainer = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	flex-direction: column;
 `
 
 const currentDay = new Date().getDay()
@@ -17,6 +20,12 @@ function App({ c = currentDay, n = nextDay, fi = fetchInfo, fs = fetchSchedule }
 	return (
 		<ScheduleProvider currentDay={c} nextDay={n} fetchSchedule={fs} fetchInfo={fi}>
 			<AppContainer>
+				<LogoInner>
+					<LogoContainer>
+						<Logo />
+					</LogoContainer>
+				</LogoInner>
+
 				<Schedule />
 			</AppContainer>
 		</ScheduleProvider>
