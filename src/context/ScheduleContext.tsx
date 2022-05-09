48e0256a71e0ad8c17b044b4de12aeade3931606
Andrew Boev <ndrwbv@gmail.com>
2022-05-09 21:05:29 +0700

    changes date for safari

--- src/context/ScheduleContext.tsx
+++ src/context/ScheduleContext.tsx
@@ -159,8 +159,8 @@ export const ScheduleProvider = ({ children, fetchSchedule, currentDay, nextDay,
 		today.setHours(0, 0, 0, 0)
 
 		return holidays.filter(holiday => {
-			const start = new Date(`${holiday.start}.${today.getFullYear()}`)
-			const end = new Date(`${holiday.end}.${today.getFullYear()}`)
+			const start = new Date(`${today.getFullYear()}-${holiday.start}`)
+			const end = new Date(`${today.getFullYear()}-${holiday.end}`)
 
 			if (today <= end && today >= start) {
 				return true
