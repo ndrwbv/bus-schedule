4ac4c5d1f294e9f163693f47094026919fd3a5c0
Andrew Boev <ndrwbv@gmail.com>
2022-10-04 18:21:30 +0700

    moves i18n to shared

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { initGA } from 'helpers/analytics'
 import YM from 'YM'
 
 import './index.css'
-import { configureI18next } from 'i18n/config.i18next'
+import { configureI18next } from 'shared/lib'
 import Game from 'Game/Game'
 import Intro from 'Game/Intro/Intro'
 
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './request/base'
 export * from './request/types'
+export * from './i18n/config.i18next'
