1d590404f35a99e404d86f1f7e9858ba8f81d450
Andrew Boev <ndrwbv@gmail.com>
2022-10-23 20:35:51 +0700

    moves Logo to entity

--- src/entities/Logo/ui/Logo.tsx
+++ src/entities/Logo/ui/Logo.tsx
@@ -3,12 +3,10 @@ import SVG from 'react-inlinesvg'
 import { ImageWrapper } from 'shared/ui/ImageWrapper'
 import LogoWithText from '../img/LogoWithText.svg'
 
-const Logo = () => {
+export const Logo = () => {
 	return (
 		<ImageWrapper w={140} h={27}>
 			<SVG src={LogoWithText} width={140} height={27} uniquifyIDs={true} />
 		</ImageWrapper>
 	)
 }
-
-export default Logo
