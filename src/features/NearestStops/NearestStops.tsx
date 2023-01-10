885f6a5d8e3918735628dd9d4301a4566fa04753
Andrew Boev <ndrwbv@gmail.com>
2023-01-10 19:50:50 +0700

    adds click event on nearest stops

--- src/features/NearestStops/NearestStops.tsx
+++ src/features/NearestStops/NearestStops.tsx
@@ -1,7 +1,8 @@
 import { useCallback, useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { userLocationSelector } from 'features/MyLocation/model/myLocationSlice'
 import L from 'leaflet'
+import { setBusStopNew } from 'shared/store/busStop/busStopInfoSlice'
 import { STOPS } from 'shared/store/busStop/const/stops'
 import { DirectionsNew, IStops } from 'shared/store/busStop/Stops'
 import { ContainerStyled } from 'shared/ui'
@@ -25,25 +26,27 @@ const NearestStop: React.FC<INearestStopProps> = ({ direction, label, id, onClic
 }
 export const NearestStops: React.FC = () => {
 	const userLocation = useSelector(userLocationSelector)
-
+	const dispatch = useDispatch()
 	const [stops, setStops] = useState<IStops<DirectionsNew>[]>([])
 
 	useEffect(() => {
 		if (userLocation) {
 			const nearestStops = STOPS.filter(
 				stop =>
-					stop.latLon &&
 					L.latLng([userLocation.coords.latitude, userLocation.coords.longitude]).distanceTo(stop.latLon) <
-						MAX_DISTANCE,
+					MAX_DISTANCE,
 			)
 
 			setStops(nearestStops)
 		}
 	}, [userLocation])
 
-	const onStopClick = useCallback((id: string) => {
-		console.log(id)
-	}, [])
+	const onStopClick = useCallback(
+		(id: string) => {
+			dispatch(setBusStopNew(id))
+		},
+		[dispatch],
+	)
 
 	if (stops.length === 0) return null
 
