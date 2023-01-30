681b96d8483fee4e3ae414e95cfa242431c0d5af
Andrew Boev <ndrwbv@gmail.com>
2023-01-30 13:21:37 +0700

    change translation link

--- src/shared/common/strings.ts
+++ src/shared/common/strings.ts
@@ -1,6 +1,6 @@
 export const COPYRIGHT = `© Andrew Boev & Friends`
 export const AVTOTRANS = `tomskavtotrans.ru`
-export const TRANSLATION_LINK = `https://www.youtube.com/watch?v=de_-FTyC4l8&ab_channel=%D0%93%D0%A2%D0%A0%D0%9A%C2%AB%D0%A2%D0%BE%D0%BC%D1%81%D0%BA%C2%BB`
+export const TRANSLATION_LINK = `https://www.youtube.com/@gtrktomsk/featured`
 export const TG_LINK = `https://t.me/ndrwbv`
 export const FEEDBACK_LINK = `https://docs.google.com/forms/d/1CIuACPCB373hVzxHdHsXbjFCkeEA2H7h7IK-CURqh2o/viewform?edit_requested=true`
 export const AVTOTRANS_LINK = `https://tomskavtotrans.ru/2022/06/30/%d1%82%d0%be%d0%bc%d1%81%d0%ba-%d0%b1%d0%be%d1%80%d0%b8%d0%ba%d0%b8-%d1%81%d0%b5%d0%b2%d0%b5%d1%80%d0%bd%d1%8b%d0%b9-%d0%bf%d0%b0%d1%80%d0%ba`
