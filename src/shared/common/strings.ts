b03e4a43f1b5d3fd9e8c1eda0b1cfab1788dc5c0
Andrew Boev <ndrwbv@gmail.com>
2023-05-29 12:13:49 +0700

    change about link

--- src/shared/common/strings.ts
+++ src/shared/common/strings.ts
@@ -4,5 +4,5 @@ export const TRANSLATION_LINK = `https://www.youtube.com/@gtrktomsk/featured`
 export const TG_LINK = `https://t.me/ndrwbv`
 export const FEEDBACK_LINK = `https://docs.google.com/forms/d/1CIuACPCB373hVzxHdHsXbjFCkeEA2H7h7IK-CURqh2o/viewform?edit_requested=true`
 export const AVTOTRANS_LINK = `https://tomskavtotrans.ru/2022/06/30/%d1%82%d0%be%d0%bc%d1%81%d0%ba-%d0%b1%d0%be%d1%80%d0%b8%d0%ba%d0%b8-%d1%81%d0%b5%d0%b2%d0%b5%d1%80%d0%bd%d1%8b%d0%b9-%d0%bf%d0%b0%d1%80%d0%ba`
-export const ABOUT_LINK = `https://wobbly-gauge-c1c.notion.site/Sever-Bus-9205a033327440b797956717f9fca366`
+export const ABOUT_LINK = `https://ndrwbv.notion.site/C-9205a033327440b797956717f9fca366`
 export const BETA_TESTER_LINK = `https://forms.gle/YsVTg9UHLAB5UEN47`
