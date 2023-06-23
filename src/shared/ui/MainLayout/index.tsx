8cb901307d2f0fdac160c3224a868bc7bd61c9c9
Andrew Boev <ndrwbv@gmail.com>
2023-06-23 14:00:15 +0700

    fix min height

--- src/shared/ui/MainLayout/index.tsx
+++ src/shared/ui/MainLayout/index.tsx
@@ -20,6 +20,7 @@ export const MainLayoutBetaStyled = styled.main`
 export const BottomSheetBgStyled = styled.div<{ bg?: string }>`
 	background: ${props => (props.bg ? props.bg : `#f4f4f2`)};
 	border-radius: 25px 25px 0 0;
+	min-height: 60vh;
 
 	@media all and (min-width: 766px) {
 		border-radius: 34px 34px 0 0;
