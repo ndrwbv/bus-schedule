308f7ea97faa081434d8ddba589b500599d4da56
Andrew Boev <ndrwbv@gmail.com>
2024-01-26 22:03:09 +0700

    changes color of game button

--- src/page/BusDriverGame/shared/ui/GameButton/GameButton.styles.ts
+++ src/page/BusDriverGame/shared/ui/GameButton/GameButton.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const GameButtonStyled = styled.button`
 	width: 100%;
-	background-color: #7de49a;
+	background-color: #ff9714;
 	padding: 20px;
 	border-radius: 25px;
 
