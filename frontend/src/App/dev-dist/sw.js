7846901cf7bc846763ae7e65e54522d15f83353a
Andrew Boev <a.boev@dodobrands.io>
2026-04-02 14:59:21 +0700

    yarn

--- frontend/src/App/dev-dist/sw.js
+++ frontend/src/App/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-9c89b58b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hds7pbltlv4"
+    "revision": "0.3fe68hlvnek"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
